4e17f90f4e32c26e2409127ea4c45da4ae3d3ba7
Acer <m.gugan2a.ictsmksm@gmail.com>
2023-01-02 23:38:12 +0800

    Visitor Resident

---
+++ app/src/main/java/com/example/workshop/ViewVisitorManagement.java
@@ -0,0 +1,14 @@
+package com.example.workshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewVisitorManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_visitor_management);
+    }
+}
